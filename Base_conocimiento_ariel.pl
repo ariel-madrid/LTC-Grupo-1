dd46507900de32835f77a7e406fd0165638c4873
ariel-madrid <ariel.argomedo@usach.cl>
2022-04-12 17:57:34 -0400

    10-20

---
+++ Base_conocimiento_ariel.pl
@@ -0,0 +1,10 @@
+juego(super_mario_odyssey,plataforma,2018).
+juego(fortnite,accion,2017).
+juego(assassins_creed_odyssey,aventura,2018).
+juego(fifa_11,deporte,2010).
+juego(forza_horizon_4,coches,2018).
+juego(dead_space,terror,2011).
+juego(bloodborne,accion,2015).
+juego(rocket_league,deporte,2015).
+juego(red_dead_redemption,aventura,2010).
+juego(cuphead,plataforma,2017).
