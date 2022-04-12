e860c1691c87ee43f2bdfc331935342e8528f186
vanessa <vanessa.vida.a@usach.cl>
2022-04-12 00:03:30 -0400

    Base conocimiento 90-00
    10 juegos con categoria y año de los 90's

---
+++ Base_conocimiento_vanessa.py
@@ -0,0 +1,10 @@
+juego(Super Mario World, plataforma, 1990).
+juego(Resident Evil, terror, 1996).
+juego(Final Fantasy VII, aventura, 1997).
+juego(Gran Turismo, coches, 1997).
+juego(Metal Gear Solid, accion, 1998).
+juego(Silent Hill, terror, 1999).
+juego(The Legend of Zelda: Ocarina of Time, aventura, 1998).
+juego(Sonic the Hedgehog, plataforma, 1991).
+juego(Mario Kart, coches, 1992).
+juego(Virtua Tennis, deporte, 1999).
