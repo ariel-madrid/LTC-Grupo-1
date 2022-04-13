6f39aa988cf5c7b98416d1f354e42c274dadc303
Alma Vidal <alma.vidal@usach.cl>
2022-04-12 19:41:04 -0600

    Base conocimiento 00-10

---
+++ Base_conocimiento_alma.pl
@@ -0,0 +1,20 @@
+# Categorias:
+# - Acción
+# - Aventura
+# - Plataforma
+# - Deporte
+# - Coches
+# - Terror
+
+# Años 00-10
+
+juego(Deus_Ex,accion,2000).
+juego(Call_of_Duty,accion,2003).
+juego(ICO,aventura,2002).
+juego(Sonic_Rush,plataforma,2005).
+juego(Pro_Evolution_Soccer_6,deporte,2006).
+juego(Project_Gotham_Tacing_4,coches,2007).
+juego(Colin_McRae_Rally_04,coches,2004).
+juego(Alan_Wake,terror,2010).
+juego(Project_Zero,terror,2001).
+juego(Half-Life_2,accion,2004).
