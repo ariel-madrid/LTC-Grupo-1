aabf790daa21ee85acbdacb43077d0df543e860e
ariel-madrid <ariel.argomedo@usach.cl>
2022-04-17 15:50:22 -0400

    Se realizan modificaciones a la base de conocimientos

--- base_conocimiento.pl
+++ base_conocimiento.pl
@@ -1,5 +1,5 @@
-#Categorias:
-# - Acción
+/*#Categorias:
+# - Accion
 # - Aventura
 # - Plataforma
 # - Deporte
@@ -9,69 +9,106 @@
 #any% -novato
 #100% -experto
 
-# Año 90-00
-juego(super_mario_world,plataforma,1990,any%,81).
-juego(super_mario_world,plataforma,1990,100%,33).
-juego(resident_evil,terror,1996,any%,98).
-juego(resident_evil,terror,1996,100%,44).
-juego(final_fantasy_vii,aventura,1997,any%,397).
-juego(final_fantasy_vii,aventura,1997,100%,1067).
-juego(gran_turismo,coches,1997,any%,92).
-juego(gran_turismo,coches,1997,100%,0).
-juego(metal_gear_solid,accion,1998,any%,35).
-juego(metal_gear_solid,accion,1998,100%,60).
-juego(silent_hill,terror,1999,any%,29).
-juego(silent_hill,terror,1999,100%,31).
-juego(the_legend_of_zelda:ocarina_of_time,aventura,1998,any%,6).
-juego(the_legend_of_zelda:ocarina_of_time,aventura,1998,100%,230).
-juego(sonic_the_hedgehog,plataforma,1991,any%,9).
-juego(sonic_the_hedgehog,plataforma,1991,100%,20).
-juego(mario_kart,coches,1992,any%,31).
-juego(mario_kart,coches,1992,100%,29).
-juego(virtua_tennis,deporte,1999,any%,0).
-juego(virtua_tennis,deporte,1999,100%,2).
+# Año 90-00*/
+juego("Super Mario World","plataforma",1990,"any%",81).
+juego("Super Mario World","plataforma",1990,"100%",33).
+juego("Resident Evil","terror",1996,"any%",98).
+juego("Resident Evil","terror",1996,"100%",44).
+juego("Final Fantasy VII","aventura",1997,"any%",397).
+juego("Final Fantasy VII","aventura",1997,"100%",1067).
+juego("Gran Turismo","coches",1997,"any%",92).
+juego("Gran Turismo","coches",1997,"100%",0).
+juego("Metal Gear Solid","accion",1998,"any%",35).
+juego("Metal Gear Solid","accion",1998,"100%",60).
+juego("Silent Hill","terror",1999,"any%",29).
+juego("Silent Hill","terror",1999,"100%",31).
+juego("The Legend of Zelda:Ocarina of Time","aventura",1998,"any%",6).
+juego("The Legend of Zelda:Ocarina of Time","aventura",1998,"100%",230).
+juego("Sonic the Hedgehog","plataforma",1991,"any%",9).
+juego("Sonic the Hedgehog","plataforma",1991,"100%",20).
+juego("Mario Kart","coches",1992,"any%",31).
+juego("Mario Kart","coches",1992,"100%",29).
+juego("Virtua Tennis","deporte",1999,"any%",0).
+juego("Virtua Tennis","deporte",1999,"100%",2).
 
-#Año 00-10
-juego(deus_ex,accion,2000,any%,30).
-juego(deus_ex,accion,2000,100%,88).
-juego(call_of_duty,accion,2003,any%,89).
-juego(call_of_duty,accion,2003,100%,276).
-juego(ico,aventura,2002,any%,115).
-juego(ico,aventura,2002,100%,214).
-juego(sonic_rush,plataforma,2005,any%,0).
-juego(sonic_rush,plataforma,2005,100%,133).
-juego(pro_evolution_Soccer_6,deporte,2006,any%,163).
-juego(pro_evolution_Soccer_6,deporte,2006,100%,0).
-juego(project_gotham_racing_4,coches,2007,any%,172).
-juego(project_gotham_racing_4,coches,2007,100%,0).
-juego(colin_mcRae_rally_04,coches,2004).
-juego(alan_wake,terror,2010).
-juego(project_zero,terror,2001).
-juego(half-life_2,accion,2004).
+/*#Año 00-10*/
+juego("Deus ex","accion",2000,"any%",30).
+juego("Deus ex","accion",2000,"100%",88).
+juego("Call of Duty","accion",2003,"any%",89).
+juego("Call of Duty","accion",2003,"100%",276).
+juego("Ico","aventura",2002,"any%",115).
+juego("Ico","aventura",2002,"100%",214).
+juego("Metal Gear Solid 2 Substance","aventura",2002,"any%",60).
+juego("Metal Gear Solid 2 Substance","aventura",2002,"100%",97).
+juego("Kirby Squeak Squad","plataforma",2006,"any%",38).
+juego("Kirby Squeak Squad","plataforma",2006,"100%",57).
+juego("Sonic Rush","plataforma",2005,"any%",0).
+juego("Sonic Rush","plataforma",2005,"100%",133).
+juego("Pro Evolution Soccer 6","deporte",2006,"any%",163).
+juego("Pro Evolution Soccer 6","deporte",2006,"100%",0).
+juego("Project gotham racing 4","coches",2007,"any%",172).
+juego("Project gotham racing 4","coches",2007,"100%",0).
+juego("Colin McRae Rally 04","coches",2007,"any%",20).
+juego("Colin McRae Rally 04","coches",2007,"100%",54).
+juego("Alan Wake","terror",2010,"any%",153).
+juego("Alan Wake","terror",2010,"100%",0).
 
-#Año 11-...
-juego(super_mario_odyssey,plataforma,2018,any%,57).
-juego(super_mario_odyssey,plataforma,2018,100%,513).
-juego(elden_ring,accion,2022,any%,26). 
-juego(elden_ring,accion,2022,100%,238).  
-juego(assassins_creed_odyssey,aventura,2018,any%,195).
-juego(assassins_creed_odyssey,aventura,2018,100%,377).
-juego(nba_2k21,deporte,2010,any%,4). 
-juego(nba_2k21,deporte,2010,100%,71).  
+/*#Año 11-...*/
+juego("Super Mario Odyssey","plataforma",2018,"any%",57).
+juego("Super Mario Odyssey","plataforma",2018,"100%",513).
+juego("Elden Ring","accion",2022,"any%",26).
+juego("Elden Ring","accion",2022,"100%",238).
+juego("Assassins Creed Odyssey","aventura",2018,"any%",195).
+juego("Assassins Creed Odyssey","aventura",2018,"100%",377).
+juego("NBA 2k21","deporte",2010,"any%",4).
+juego("NBA 2k21","deporte",2010,"100%",71).
 
-#En categoria All Drag Races 
-juego(forza_horizon_4,coches,2018,any%,8).
-#En categoria Britain-All Races 
-juego(forza_horizon_4,coches,2018,100%,240).
+/*#En categoria All Drag Races*/
+juego("Forza Horizon 4","coches",2018,"any%",8).
+/*#En categoria Britain-All Races*/
+juego("Forza Horizon 4","coches",2018,"100%",240).
 
-juego(dead_space,terror,2011,any%,62).
-juego(dead_space,terror,2011,100%,121).
-juego(bloodborne,accion,2015,any%,21).
-juego(bloodborne,accion,2015,100%,62).
-juego(rocket_league,deporte,2015,any%,0).
-juego(rocket_league,deporte,2015,100%,57).
-juego(red_dead_redemption,aventura,2010,any%,267).
-juego(red_dead_redemption,aventura,2010,100%,561).
-juego(cuphead,plataforma,2017,any%,17).
-juego(cuphead,plataforma,2017,100%,33).
+juego("Dead Space","terror",2011,"any%",62).
+juego("Dead Space","terror",2011,"100%",121).
+juego("Bloodborne","accion",2015,"any%",21).
+juego("Bloodborne","accion",2015,"100%",62).
+juego("Rocket League","deporte",2015,"any%",0).
+juego("Rocket League","deporte",2015,"100%",57).
+juego("Red Dead Redemption","aventura",2010,"any%",267).
+juego("Red Dead Redemption","aventura",2010,"100%",561).
+juego("Cuphead","plataforma",2017,"any%",17).
+juego("Cuphead","plataforma",2017,"100%",33).
+
+/*Se a�aden algunos juegos extras para aumentar la base de conocimientos*/
+juego("Donkey Kong Country","plataforma",1994,"any%",7).
+juego("Donkey Kong Country","plataforma",1994,"100%",30).
+juego("Metroid Dread","accion",2021,"any%",71).
+juego("Metroid Dread","accion",2021,"100%",108).
+
+
+/*�Con cuanto tiempo dispone para dedicar al videojuego?
+
+	# - Poco
+	# - Prefiero un juego de mediana duracion
+	# - No es relevante
+
+#�Es de su preferencia alguna decada en especifico?
+
+	# - 1990 - 2000 - 2010 - 2020
+
+#�Que tan habil se considera?
+
+	# - Novato (any%)
+	# - Experto (100%)
+
+#�Que tipo de juego prefiere?
+
+	# - Desafiante
+	# - Prefiero explorar
+	# - Disparos
+	# - Me gustan los coches
+	# - Cualquiera
+*/
+
+
 
