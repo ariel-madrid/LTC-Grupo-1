917261ad9d11af97eabc0265cae574cea3413d75
ariel-madrid <ariel.argomedo@usach.cl>
2022-04-26 22:35:54 -0400

    -

--- base_conocimiento.pl
+++ base_conocimiento.pl
@@ -44,26 +44,78 @@ juego("Virtua Tennis","deporte",1999,"any",0).
 juego("Virtua Tennis","deporte",1999,"100",2).
 
 % Año 00-10 %
-juego("Deus ex","accion",2000,"any",30).
-juego("Deus ex","accion",2000,"100",88).
-juego("Call of Duty","accion",2003,"any",89).
-juego("Call of Duty","accion",2003,"100",276).
-juego("Ico","aventura",2002,"any",115).
-juego("Ico","aventura",2002,"100",214).
-juego("Metal Gear Solid 2 Substance","aventura",2002,"any",60).
-juego("Metal Gear Solid 2 Substance","aventura",2002,"100",97).
-juego("Kirby Squeak Squad","plataforma",2006,"any",38).
-juego("Kirby Squeak Squad","plataforma",2006,"100",57).
-juego("Sonic Rush","plataforma",2005,"any",0).
-juego("Sonic Rush","plataforma",2005,"100",133).
-juego("Pro Evolution Soccer 6","deporte",2006,"any",163).
-juego("Pro Evolution Soccer 6","deporte",2006,"100",0).
-juego("Project gotham racing 4","coches",2007,"any",172).
-juego("Project gotham racing 4","coches",2007,"100",0).
-juego("Colin McRae Rally 04","coches",2007,"any",20).
-juego("Colin McRae Rally 04","coches",2007,"100",54).
-juego("Alan Wake","terror",2010,"any",153).
-juego("Alan Wake","terror",2010,"100",0).
+juego(Deus_Ex,accion,2000).
+juego(Call_of_Duty,accion,2003).
+juego(ICO,aventura,2002).
+juego(Sonic_Rush,plataforma,2005).
+juego(Pro_Evolution_Soccer_6,deporte,2006).
+juego(Project_Gotham_Tacing_4,coches,2007).
+juego(Colin_McRae_Rally_04,coches,2004).
+juego(Alan_Wake,terror,2010).
+juego(Project_Zero,terror,2001).
+juego(Half-Life_2,accion,2004).
+juego("Deus ex","accion",2000,"any%",30).
+juego("Deus ex","accion",2000,"100%",88).
+juego("Call of Duty","accion",2003,"any%",89).
+juego("Call of Duty","accion",2003,"100%",276).
+juego("Demon's Souls","accion",2009,"any%",45).
+juego("Demon's Souls","accion",2009,"100%",146).
+juego("Xenoblade Chronicles","accion",2010,"any%",294).
+juego("Xenoblade Chronicles","accion",2010,"100%",1536).
+juego("Kingdom Hearts","accion",2002,"any%",311).
+juego("Kingdom Hearts","accion",2002,"100%",748).
+juego("Ico","aventura",2002,"any%",115).
+juego("Ico","aventura",2002,"100%",214).
+juego("Metal Gear Solid 2 Substance","aventura",2002,"any%",60).
+juego("Metal Gear Solid 2 Substance","aventura",2002,"100%",97).
+juego("Metroid Prime: Hunters","aventura",2006,"any%",69).
+juego("Metroid Prime: Hunters","aventura",2006,"100%",112).
+juego("Far Cry 2","aventura",2008,"any%",130).
+juego("Far Cry 2","aventura",2008,"100%",0).
+juego("Dragon Quest Heroes: Rocket Slime","aventura",2005,"any%",124).
+juego("Dragon Quest Heroes: Rocket Slime","aventura",2005,"100%",194).
+juego("Kirby Squeak Squad","plataforma",2006,"any%",38).
+juego("Kirby Squeak Squad","plataforma",2006,"100%",57).
+juego("Sonic Rush","plataforma",2005,"any%",0).
+juego("Sonic Rush","plataforma",2005,"100%",133).
+juego("La leyenda de Spyro: la fuerza del dragón","plataforma",2008,"any%",39). 
+juego("La leyenda de Spyro: la fuerza del dragón","plataforma",2008,"100%",73).
+juego("New Super Mario Bros. Wii","plataforma",2009,"any%",22). 
+juego("New Super Mario Bros. Wii","plataforma",2009,"100%",218).
+juego("Mega Man ​​Xtreme","plataforma",2000,"any%",23). 
+juego("Mega Man ​​Xtreme","plataforma",2000,"100%",23).
+juego("Pro Evolution Soccer 6","deporte",2006,"any%",163).
+juego("Pro Evolution Soccer 6","deporte",2006,"100%",0).
+juego("Punch-Out!!","deporte",2009,"any%",21).
+juego("Punch-Out!!","deporte",2009,"100%",0).
+juego("Dave Mirra Freestyle BMX","deporte",2000,"any%",25).
+juego("Dave Mirra Freestyle BMX","deporte",2000,"100%",37).
+juego("Kelly Slater's Pro Surfer","deporte",2002,"any%",49).
+juego("Kelly Slater's Pro Surfer","deporte",2002,"100%",0).
+juego("Xtreme Sports","deporte",2000,"any%",6).
+juego("Xtreme Sports","deporte",2000,"100%",12).
+juego("Project gotham racing 4","coches",2007,"any%",172).
+juego("Project gotham racing 4","coches",2007,"100%",0).
+juego("Colin McRae Rally 04","coches",2007,"any%",20).
+juego("Colin McRae Rally 04","coches",2007,"100%",54).
+juego("Need for speed: Most wanted","coches",2005,"any%",205).
+juego("Need for speed: Most wanted","coches",2005,"100%",464).
+juego("Need for speed: Carbon","coches",2006,"any%",111).
+juego("Need for speed: Carbon","coches",2006,"100%",170).
+juego("Diddy Kong Racing DS","coches",2007,"any%",153).
+juego("Diddy Kong Racing DS","coches",2007,"100%",161).
+juego("Alan Wake","terror",2010,"any%",153).
+juego("Alan Wake","terror",2010,"100%",0).
+juego("Left 4 Dead 2","terror",2009,"any%",46).
+juego("Left 4 Dead 2","terror",2009,"100%",146).
+juego("The House of the Dead 4","terror",2005,"any%",17). #easy
+juego("The House of the Dead 4","terror",2005,"100%",19). #very hard
+juego("Red Dead Redemption: Undead Nightmare","terror",2010,"any%",54). 
+juego("Red Dead Redemption: Undead Nightmare","terror",2010,"100%",201).
+juego("BioShock","terror",2007,"any%",28). 
+juego("BioShock","terror",2007,"100%",76).
+juego("Penumbra: Black Plague","terror",2008,"any%",11). 
+juego("Penumbra: Black Plague","terror",2008,"100%",18).
 
 % Anho 11-... %
 juego("Super Mario Odyssey","plataforma",2018,"any",57).
@@ -74,12 +126,8 @@ juego("Assassins Creed Odyssey","aventura",2018,"any",195).
 juego("Assassins Creed Odyssey","aventura",2018,"100",377).
 juego("NBA 2k21","deporte",2010,"any",4).
 juego("NBA 2k21","deporte",2010,"100",71).
-
-% En categoria All Drag Races %
 juego("Forza Horizon 4","coches",2018,"any",8).
-% En categoria Britain-All Races %
 juego("Forza Horizon 4","coches",2018,"100",240).
-
 juego("Dead Space","terror",2011,"any",62).
 juego("Dead Space","terror",2011,"100",121).
 juego("Bloodborne","accion",2015,"any",21).
@@ -90,6 +138,48 @@ juego("Red Dead Redemption","aventura",2010,"any",267).
 juego("Red Dead Redemption","aventura",2010,"100",561).
 juego("Cuphead","plataforma",2017,"any",17).
 juego("Cuphead","plataforma",2017,"100",33).
+juego("Metroid Dread","accion",2021,"any%",71).
+juego("Metroid Dread","accion",2021,"100%",108).
+juego("Grand Theft Auto V","accion",2013,"any%",332).
+juego("Grand Theft Auto V","accion",2013,"100%",571).
+juego("Dark Souls III","accion",2016,"any%",31).
+juego("Dark Souls III","accion",2016,"100%",72).
+juego("Outlast","terror",2013,"any%",8).
+juego("Outlast","terror",2013,"100%",28).
+juego("Resident Evil 3","terror",2020,"any%",42).
+juego("Resident Evil 3","terror",2020,"100%",61).
+juego("Five Nights at Freddy's","terror",2014,"any%",45).
+juego("Five Nights at Freddy's","terror",2014,"100%",64).
+juego("The Legend of Zelda: Breath of the Wild","aventura",2017,"any%",24).
+juego("The Legend of Zelda: Breath of the Wild","aventura",2017,"100%",1008).
+juego("The Witcher 3: Wild Hunt","aventura",2015,"any%",169).
+juego("The Witcher 3: Wild Hunt","aventura",2015,"100%",257).
+juego("Journey to the Savage Planet","aventura",2020,"any%",10).
+juego("Journey to the Savage Planet","aventura",2020,"100%",165).
+juego("Geometry Dash","plataforma",2013,"any%",32).
+juego("Geometry Dash","plataforma",2013,"100%",32).
+juego("Kirby: Star Allies","plataforma",2018,"any%",80).
+juego("Kirby: Star Allies","plataforma",2018,"100%",221).
+juego("Super Mario 3D World","plataforma",2013,"any%",95).
+juego("Super Mario 3D World","plataforma",2013,"100%%",444).
+juego("Need for Speed: Rivals","coches",2013,"any%",56).
+juego("Need for Speed: Rivals","coches",2013,"any%",393).
+juego("Crash Team Racing Nitro-Fueled","coches",2019,"any%",45).
+juego("Crash Team Racing Nitro-Fueled","coches",2019,"100%",52).
+juego("Inazuma Eleven Strikers","deporte",2011,"any%",69).
+juego("Inazuma Eleven Strikers","deporte",2011,"100%",98).
+juego("Mario Golf: Super Rush","deporte",2021,"any%",201).
+juego("Mario Golf: Super Rush","deporte",2021,"100%",260).
+juego("Mario Tennis Aces","deporte",2018,"any%",64).
+juego("Mario Tennis Aces","deporte",2018,"any%",81).
+juego("Hollow Knight","accion",2017,"any%",32).
+juego("Hollow Knight","accion",2017,"100%",143).
+juego("Ori and the Blind Forest","plataforma",2015,"any%",22).
+juego("Ori and the Blind Forest","plataforma",2015,"100%",68).
+juego("What Remains of Edith Finch","plataforma",2017,"any%",35).
+juego("What Remains of Edith Finch","plataforma",2017,"100%",61).
+juego("DOOM","accion",2016,"any%",18).
+juego("DOOM","accion",2016,"100%",129).
 
 % �Con cuanto tiempo dispone para dedicar al videojuego?
 
