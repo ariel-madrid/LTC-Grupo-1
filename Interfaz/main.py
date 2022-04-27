b768b6e50ae1dfec2d755bc9263fe2ddf1f3344c
ariel-madrid <ariel.argomedo@usach.cl>
2022-04-27 17:39:46 -0400

    Se añaden reglas

--- Interfaz/main.py
+++ Interfaz/main.py
@@ -1,109 +1,196 @@
+#Se importan librerias 
 import tkinter
 from tkinter import ttk
-nombre = ""
+from tkinter import messagebox
+from pyswip import *
+
+prolog = Prolog()
+prolog.consult("../base_conocimiento.pl")
 
 def capturarInformacion(combo,combo2,radioValue,combo3):
+    juegos = []
+
     preferenciaTiempo = combo.get()
     preferenciaDecada = combo2.get()
     preferenciaNivelHabilidad = radioValue.get()
     preferenciaCategoria = combo3.get()
-    print(preferenciaTiempo)        
-    print(preferenciaDecada)    
-    print(preferenciaNivelHabilidad)
-    print(preferenciaCategoria)
-def ingresoSistema():
+
+    #Dependiendo de las entradas a traves de la interfaz, se realizan diferentes consultas a la base de conocimientos.
+    if (preferenciaNivelHabilidad == ""):
+        tkinter.messagebox.showinfo(message="Debe seleccionar un nivel de habilidad", title="Alerta")
+    if (preferenciaTiempo == "Menos de 30 minutos"):
+            preferenciaTiempo = "Corta"
+    elif (preferenciaTiempo == "Entre 30 y 90 minutos"):
+        preferenciaTiempo = "Media"
+    elif (preferenciaTiempo == "Más de 90 minutos"):
+        preferenciaTiempo = "Larga"
+    else:
+        preferenciaTiempo = "Seleccione"
+
+    if (preferenciaDecada == "1990-1999"):
+        preferenciaDecada = "90"
+    elif (preferenciaDecada == "2000-2009"):
+        preferenciaDecada = "2000"
+    elif (preferenciaDecada == "2010-2029"):
+        preferenciaDecada = "10"
+    else:
+        preferenciaDecada = "Seleccione"
+
+    if (preferenciaNivelHabilidad == "Novato"):
+        preferenciaNivelHabilidad = "any"
+    elif (preferenciaNivelHabilidad == "Experto"):
+        preferenciaNivelHabilidad = "100"
+    else:
+        preferenciaNivelHabilidad = ""
+
+    if (preferenciaCategoria == "Accion"):
+        preferenciaCategoria = "accion"
+    elif (preferenciaCategoria == "Aventura"):
+        preferenciaCategoria = "aventura"
+    elif (preferenciaCategoria == "Plataforma"):
+        preferenciaCategoria = "plataforma"
+    elif (preferenciaCategoria == "Deporte"):
+        preferenciaCategoria = "deporte"
+    elif (preferenciaCategoria == "Coches"):
+        preferenciaCategoria = "coches"
+    elif (preferenciaCategoria == "Terror"):
+        preferenciaCategoria = "terror"
+    else:
+        preferenciaCategoria = "Seleccione"
+        
+    #En caso de que sean seleccionados todos los campos
+    if (preferenciaTiempo != "Seleccione" and preferenciaDecada != "Seleccione" and preferenciaNivelHabilidad != "" and preferenciaCategoria != "Seleccione"):
+        
+        for consulta in prolog.query("seleccionarJuegoCDHT(" + '"'+preferenciaCategoria+'"' + "," + '"'+preferenciaDecada+'"' + "," '"'+preferenciaNivelHabilidad+'"' +","+'"'+preferenciaTiempo+'"' + ",X)"):
+            juegos.append(consulta["X"])
+    #En caso de que se seleccione Categoria, Decada y Habilidad
+    elif (preferenciaDecada != "Seleccione" and preferenciaNivelHabilidad != "" and preferenciaCategoria != "Seleccione" and preferenciaTiempo=="Seleccione"):
+        
+        for consulta in prolog.query("seleccionarJuegoCDH(" + '"'+preferenciaCategoria+'"' + "," + '"'+preferenciaDecada+'"' + "," '"'+preferenciaNivelHabilidad+'"' +",X)"):
+            juegos.append(consulta["X"])
+    #En caso de que ingrese Decada, Habilidad, Tiempo.
+    elif (preferenciaDecada != "Seleccione" and preferenciaNivelHabilidad != "" and preferenciaTiempo != "Seleccione" and preferenciaCategoria == "Seleccione"):
+        
+        for consulta in prolog.query("seleccionarJuegoDHT(" +'"'+preferenciaDecada+'"'+","+'"'+preferenciaNivelHabilidad+'"' +","+'"'+preferenciaTiempo+'"'+",X)"):
+            juegos.append(consulta["X"])
+    #En caso de que ingrese Categoria, Habilidad, Tiempo
+    elif (preferenciaCategoria != "Seleccione" and preferenciaNivelHabilidad != "" and preferenciaTiempo != "Seleccione"):
+        
+        for consulta in prolog.query("seleccionarJuegoCHT(" +'"'+preferenciaCategoria+'"'+","+'"'+preferenciaNivelHabilidad+'"' +","+'"'+preferenciaTiempo+'"'+",X)"):
+            juegos.append(consulta["X"])
+    #En caso que se seleccion Categoria y Habilidad
+    elif (preferenciaCategoria != "Seleccione" and preferenciaNivelHabilidad != "" and preferenciaDecada == "Seleccione" and preferenciaTiempo == "Seleccione"):
+        
+        for consulta in prolog.query("seleccionarJuegoCH(" + '"'+preferenciaCategoria+'"' + "," + '"'+preferenciaNivelHabilidad+'"' +",X)"):
+            juegos.append(consulta["X"])
+    #En caso de que ingrese Decada, Habilidad.
+    elif (preferenciaDecada != "Seleccione" and preferenciaNivelHabilidad != "" and preferenciaTiempo == "Seleccione" and preferenciaCategoria == "Seleccione"):
+    
+        for consulta in prolog.query("seleccionarJuegoDH(" +'"'+preferenciaDecada+'"'+","+'"'+preferenciaNivelHabilidad+'"' +",X)"):
+            juegos.append(consulta["X"])
+    #En caso de que ingrese Habilidad, Tiempo.
+    elif (preferenciaDecada == "Seleccione" and preferenciaNivelHabilidad != "" and preferenciaTiempo != "Seleccione" and preferenciaCategoria == "Seleccione"):
+        
+        for consulta in prolog.query("seleccionarJuegoHT(" +'"'+preferenciaNivelHabilidad+'"'+","+'"'+preferenciaTiempo+'"' +",X)"):
+            juegos.append(consulta["X"])
+    #En caso que solo se seleccione Habilidad
+    elif (preferenciaNivelHabilidad != "" and preferenciaCategoria == "Seleccione" and preferenciaDecada == "Seleccione" and preferenciaTiempo == "Seleccione"):
+        
+        for consulta in prolog.query("seleccionarJuegoH(" +'"'+preferenciaNivelHabilidad+'"' +",X)"):
+            juegos.append(consulta["X"])
+
+    tkinter.messagebox.showinfo(message=juegos, title="Alerta")
+def ingresoSistema(inputNombre,ventana):
     nombre = str(inputNombre.get())
     ventana.destroy()
 
     selecciones =  tkinter.Tk()
-    selecciones.geometry("600x400")
+    selecciones.geometry("600x450")
     selecciones.resizable(0, 0)
     selecciones.title("Seleccion de preferencias")
 
-    selecciones.columnconfigure(0, weight=2)
-    selecciones.columnconfigure(1, weight=4)
-    selecciones.columnconfigure(2, weight=4)
-
-    selecciones.rowconfigure(0, weight=2)
-    selecciones.rowconfigure(1, weight=2)
-    selecciones.rowconfigure(2, weight=2)
+    #Cargar imagen para el fondo
+    space = tkinter.PhotoImage( file = "./img/space.png")
+    small_img=space.subsample(1,1)
+    background = tkinter.Label(selecciones, image=small_img)
+    background.space = space 
+    background.place(x=0,y=0)
 
     mensaje = tkinter.Label(selecciones,text="Bienvenido "+nombre,bg = "#88cffa")
-    mensaje.grid(column=0, row=0,sticky=tkinter.N)
+    mensaje.place(x=270, y=20)
 
     tiempo = tkinter.Label(selecciones,text="¿Con cuanto tiempo cuenta para dedicar al videojuego?",bg = "#88cffa")
-    tiempo.place(x=50, y=50)
+    tiempo.place(x=50, y=60)
 
     combo = ttk.Combobox()
-    combo = ttk.Combobox(state="readonly",values=["Menos de 30 minutos","Entre 30 y 90 minutos","Más de 90 minutos"])
+    combo = ttk.Combobox(state="readonly",values=["Menos de 30 minutos","Entre 30 y 90 minutos","Más de 90 minutos","No es relevante"])
     combo.set("Seleccione")
-    combo.place(x=50, y=75)
+    combo.place(x=50, y=85)
 
     decada = tkinter.Label(selecciones,text="¿Es de su preferencia alguna decada en especifico?",bg = "#88cffa")
-    decada.place(x=50, y=110)
+    decada.place(x=50, y=120)
 
     combo2 = ttk.Combobox()
     combo2 = ttk.Combobox(state="readonly",values=["1990-1999","2000-2009","2010-2029","No es relevante"])
     combo2.set("Seleccione")
-    combo2.place(x=50, y=135)
+    combo2.place(x=50, y=145)
 
     decada = tkinter.Label(selecciones,text="¿Que tan habil se considera?",bg = "#88cffa")
-    decada.place(x=50, y=170)
+    decada.place(x=50, y=180)
 
     radioValue = tkinter.StringVar()
 
-    rdioOne = tkinter.Radiobutton(selecciones, text='Novato',
-                             variable=radioValue, value='Novato') 
-    rdioTwo = tkinter.Radiobutton(selecciones, text='Experimentado',
-                             variable=radioValue, value='Experimentado') 
-    rdioThree = tkinter.Radiobutton(selecciones, text='Experto',
+    rdioNovato = tkinter.Radiobutton(selecciones, text='Novato',
+                             variable=radioValue, value='Novato')
+    rdioExperto = tkinter.Radiobutton(selecciones, text='Experto',
                              variable=radioValue, value='Experto')
     
-    rdioOne.place(x=50, y=200)
-    rdioTwo.place(x=130, y=200)
-    rdioThree.place(x=250, y=200)
+    rdioNovato.place(x=50, y=210)
+    rdioExperto.place(x=150, y=210)
 
     categoria = tkinter.Label(selecciones,text="¿Es de su preferencia alguna categoria en especifico?",bg = "#88cffa")
-    categoria.place(x=50, y=230)
+    categoria.place(x=50, y=240)
 
     combo3 = ttk.Combobox()
-    combo3 = ttk.Combobox(state="readonly",values=["Accion","Aventura","Plataforma","Deporte","Coches","Terror"])
+    combo3 = ttk.Combobox(state="readonly",values=["Accion","Aventura","Plataforma","Deporte","Coches","Terror","No es relevante"])
     combo3.set("Seleccione")
-    combo3.place(x=50, y=255)
+    combo3.place(x=50, y=265)
 
     enviar = tkinter.Button(selecciones,text="Consultar",command=lambda: capturarInformacion(combo,combo2,radioValue,combo3))
-    enviar.place(x=510, y=330)
+    enviar.place(x=510, y=340)
 
-#Crear ventana
-ventana =  tkinter.Tk()
-ventana.geometry("600x300")
-ventana.resizable(0, 0)
-ventana.title("Bienvenido")
-#Cargar imagen para el fondo
-bg = tkinter.PhotoImage( file = "./img/mario.png")
+def root():
+    
+    #Crear ventana
+    ventana =  tkinter.Tk()
+    ventana.geometry("600x330")
+    ventana.resizable(0, 0)
+    ventana.title("Bienvenido")
+    #Cargar imagen para el fondo
+    bg = tkinter.PhotoImage( file = "./img/mario.png")
+    small_img=bg.subsample(1,1)
+    background = tkinter.Label(ventana, image=small_img)
+    background.bg = bg 
+    background.place(x=0,y=0)
 
-background = tkinter.Label(ventana, image = bg)
-background.place(x = 0,y = 0)
+    #Mensaje de bienvenida
+    bienvenida = tkinter.Label(ventana,text="Bienvenido/a al sistema de recomendacion de videojuegos",bg = "#88cffa")
+    bienvenida.place(x=140,y=40)
 
-#Configurar el grid
-ventana.columnconfigure(0, weight=4)
-ventana.columnconfigure(1, weight=8)
-ventana.columnconfigure(2, weight=4)
-ventana.rowconfigure(0, weight=4)
-ventana.rowconfigure(1, weight=4)
-ventana.rowconfigure(2, weight=4)
+    #Etiqueta 
+    name = tkinter.Label(ventana,text="Ingrese su nombre: ",bg = "#88cffa")
+    name.place(x=150,y = 150)
 
-bienvenida = tkinter.Label(ventana,text="Bienvenido/a al sistema de recomendacion de videojuegos",bg = "#88cffa")
-bienvenida.grid(column=1, row=0)
+    #Input para el nombre
+    inputNombre = tkinter.Entry(ventana)
+    inputNombre.place(x=270,y = 150)
 
-name = tkinter.Label(ventana,text="Ingrese su nombre: ",bg = "#88cffa")
-name.grid(column=0, row=1,sticky=tkinter.W, padx=8, pady=8)
+    #Boton para ingresar al sistema
+    boton = tkinter.Button(ventana,text="Ingresar",command=lambda: ingresoSistema(inputNombre,ventana))
+    boton.place(x=390,y = 205)
 
-inputNombre = tkinter.Entry(ventana)
-inputNombre.grid(column=1,row=1,padx=8, pady=8)
+    #Se muestra la ventana
+    ventana.mainloop()
 
-boton = tkinter.Button(ventana,text="Ingresar",command=lambda: ingresoSistema())
-boton.grid(column=2,row=1,sticky=tkinter.E, padx=8, pady=8)
-
-ventana.mainloop()
-
+if __name__ == "__main__":
+    root()
