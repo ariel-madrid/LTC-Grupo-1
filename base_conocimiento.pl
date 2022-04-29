b9d337ba6bd525bbcbf5abb7e380f76c83163bf5
Ariel Argomedo <ariel.argomedo@usach.cl>
2022-04-29 00:45:51 -0400

    Se añaden juegos extra a la base de conocimientos

--- base_conocimiento.pl
+++ base_conocimiento.pl
@@ -1,33 +1,58 @@
+% Año 00-99 %
 juego('Super mario world','Plataforma',"90","ANY%","Media").
 juego('Super mario world','Plataforma',"90","100%","Media").
 juego('Rayman','Plataforma',"90","ANY%","Media").
 juego('Rayman','Plataforma',"90","100%","Media").
 juego('Sonic the Hedgehog','Plataforma',"90","ANY%","Corta").
 juego('Sonic the Hedgehog','Plataforma',"90","100%","Media").
+juego('Spyro 2: Riptos Rage!','Plataforma',"90","ANY%","Corta").
+juego('Spyro 2: Riptos Rage!','Plataforma',"90","100%","Larga").
+juego('Donkey Kong Country','Plataforma',"90","ANY%","Corta").
+juego('Donkey Kong Country','Plataforma',"90","100%","Media").
+juego('Donkey Kong 64','Plataforma',"90","ANY%","Corta").
+juego('Donkey Kong 64','Plataforma',"90","100%","Larga").
+juego('Donkey Kong Country 2: Diddys Kong Quest','Plataforma',"90","ANY%","Corta").
+juego('Donkey Kong Country 2: Diddys Kong Quest','Plataforma',"90","100%","Larga").
+
 juego('Resident Evil','Terror',"90","ANY%","Larga").
 juego('Resident Evil','Terror',"90","100%","Media").
 juego('Silent Hill','Terror',"90","ANY%","Corta").
 juego('Silent Hill','Terror',"90","100%","Media").
 juego('Ultimate Doom','Terror',"90","ANY%","Corta").
 juego('Ultimate Doom','Terror',"90","100%","Corta").
+
 juego('Final Fantasy VII','Aventura',"90","ANY%","Larga").
 juego('Final Fantasy VII','Aventura',"90","100%","Larga").
 juego('The Legend of Zelda: Ocarina of Time','Aventura',"90","ANY%","Corta").
 juego('The Legend of Zelda: Ocarina of Time','Aventura',"90","100%","Larga").
-juego('Soulcalibur','Aventura',"90","ANY%","Larga").
+juego('Soulcalibur','Aventura',"90","100%","Larga").
 juego('Soulcalibur','Aventura',"90","ANY%","Corta").
+juego('Ninja Gaiden','Aventura',"90","ANY%","Corta").
+juego('Ninja Gaiden','Aventura',"90","100%","Larga").
+juego('Pokemon Snap','Aventura',"90","ANY%","Corta").
+juego('Pokemon Snap','Aventura',"90","100%","Corta").
+
+
 juego('Gran Turismo','Coches',"90","ANY%","Larga").
 juego('Gran Turismo','Coches',"90","100%","Corta").
 juego('Mario Kart','Coches',"90","ANY%","Media").
 juego('Mario Kart','Coches',"90","100%","Corta").
 juego('Crash Team Racing','Coches',"90","ANY%","Corta").
 juego('Crash Team Racing','Coches',"90","100%","Larga").
+juego('Diddy Kong Racing','Coches',"90","ANY%","Media").
+juego('Diddy Kong Racing','Coches',"90","100%","Larga").
+
 juego('Metal Gear Solid','Accion',"90","ANY%","Media").
 juego('Metal Gear Solid','Accion',"90","100%","Media").
 juego('Unreal Tournament','Accion',"90","ANY%","Corta").
 juego('Unreal Tournament','Accion',"90","100%","Larga").
 juego('Metal Slug','Accion',"90","ANY%","Corta").
 juego('Metal Slug','Accion',"90","100%","Corta").
+juego('Mega Man X','Accion',"90","ANY%","Media").
+juego('Mega Man X','Accion',"90","100%","Media").
+juego('Star Fox 64','Accion',"90","ANY%","Corta").
+juego('Star Fox 64','Accion',"90","100%","Media").
+
 juego('Virtua Tennis','Deporte',"90","ANY%","Corta").
 juego('Virtua Tennis','Deporte',"90","100%","Corta").
 
@@ -43,6 +68,9 @@ juego('Xenoblade Chronicles','Accion',"2000","ANY%","Larga").
 juego('Xenoblade Chronicles','Accion',"2000","100%","Larga").
 juego('Kingdom Hearts','Accion',"2000","ANY%","Larga").
 juego('Kingdom Hearts','Accion',"2000","100%","Larga").
+juego('Metroid Fusion','Accion',"2000","ANY%","Media").
+juego('Metroid Fusion','Accion',"2000","100%","Larga").
+
 juego('Ico','Aventura',"2000","ANY%","Larga").
 juego('Ico','Aventura',"2000","100%","Larga").
 juego('Metal Gear Solid 2 Substance','Aventura',"2000","ANY%","Media").
@@ -53,26 +81,37 @@ juego('Far Cry 2','Aventura',"2000","ANY%","Larga").
 juego('Far Cry 2','Aventura',"2000","100%","Corta").
 juego('Dragon Quest Heroes: Rocket Slime','Aventura',"2000","ANY%","Larga").
 juego('Dragon Quest Heroes: Rocket Slime','Aventura',"2000","100%","Larga").
-juego('Kirby Squeak Squad',"Plataforma","2000","ANY%","Media").
-juego('Kirby Squeak Squad',"Plataforma","2000","100%","Media").
-juego('Sonic Rush',"Plataforma","2000","ANY%","Corta").
-juego('Sonic Rush',"Plataforma","2000","100%","Larga").
-juego('La leyenda de Spyro: la fuerza del dragon',"Plataforma","2000","ANY%","Media").
-juego('La leyenda de Spyro: la fuerza del dragon',"Plataforma","2000","100%","Media").
-juego('New Super Mario Bros. Wii',"Plataforma","2000","ANY%","Corta").
-juego('New Super Mario Bros. Wii',"Plataforma","2000","100%","Larga").
-juego('Mega Man Xtreme',"Plataforma","2000","ANY%","Corta").
-juego('Mega Man Xtreme',"Plataforma","2000","100%","Corta").
+juego('Banjo-Tooie','Aventura',"2000","ANY%","Corta").
+juego('Banjo-Tooie','Aventura',"2000","100%","Larga").
+juego('The Legend of Zelda: The Minish Cap','Aventura',"2000","ANY%","Media").
+juego('The Legend of Zelda: The Minish Cap','Aventura',"2000","100%","Larga").
+
+juego('Kirby Squeak Squad','Plataforma',"2000","ANY%","Media").
+juego('Kirby Squeak Squad','Plataforma',"2000","100%","Media").
+juego('Sonic Rush','Plataforma',"2000","ANY%","Corta").
+juego('Sonic Rush','Plataforma',"2000","100%","Larga").
+juego('La leyenda de Spyro: la fuerza del dragon','Plataforma',"2000","ANY%","Media").
+juego('La leyenda de Spyro: la fuerza del dragon','Plataforma',"2000","100%","Media").
+juego('New Super Mario Bros. Wii','Plataforma',"2000","ANY%","Corta").
+juego('New Super Mario Bros. Wii','Plataforma',"2000","100%","Larga").
+juego('Mega Man Xtreme','Plataforma',"2000","ANY%","Corta").
+juego('Mega Man Xtreme','Plataforma',"2000","100%","Corta").
+juego('Super Meat Boy','Plataforma',"2000","ANY%","Corta").
+juego('Super Meat Boy','Plataforma',"2000","100%","Media").
+juego('Super Monkey Ball','Plataforma',"2000","ANY%","Corta").
+juego('Super Monkey Ball','Plataforma',"2000","100%","Media").
+
 juego('Pro Evolution Soccer 6','Deporte',"2000","ANY%","Larga").
 juego('Pro Evolution Soccer 6','Deporte',"2000","100%","Corta").
 juego('Punch-Out!!','Deporte',"2000","ANY%","Corta").
 juego('Punch-Out!!','Deporte',"2000","100%","Corta").
 juego('Dave Mirra Freestyle BMX','Deporte',"2000","ANY%","Corta").
 juego('Dave Mirra Freestyle BMX','Deporte',"2000","100%","Media").
-juego('Kelly Slaters Pro Surfer','Deporte',"2000","ANY%","Me00dia").
+juego('Kelly Slaters Pro Surfer','Deporte',"2000","ANY%","Media").
 juego('Kelly Slaters Pro Surfer','Deporte',"2000","100%","Corta").
 juego('Xtreme Sports','Deporte',"2000","ANY%","Corta").
 juego('Xtreme Sports','Deporte',"2000","100%","Corta").
+
 juego('Project gotham racing 4','Coches',"2000","ANY%","Larga").
 juego('Project gotham racing 4','Coches',"2000","100%","Corta").
 juego('Colin McRae Rally 04','Coches',"2000","ANY%","Corta").
@@ -83,6 +122,7 @@ juego('Need for speed: Carbon','Coches',"2000","ANY%","Larga").
 juego('Need for speed: Carbon','Coches',"2000","100%","Larga").
 juego('Diddy Kong Racing DS','Coches',"2000","ANY%","Larga").
 juego('Diddy Kong Racing DS','Coches',"2000","100%","Larga").
+
 juego('Alan Wake','Terror',"2000","ANY%","Larga").
 juego('Alan Wake','Terror',"2000","100%","Corta").
 juego('Left 4 Dead 2','Terror',"2000","ANY%","Media").
@@ -97,65 +137,74 @@ juego('Penumbra: Black Plague','Terror',"2000","ANY%","Corta").
 juego('Penumbra: Black Plague','Terror',"2000","100%","Corta").
 
 % Anho 11-... %
-juego('Super Mario Odyssey',"Plataforma","10","ANY%","Media").
-juego('Super Mario Odyssey',"Plataforma","10","100%","Larga").
+juego('Super Mario Odyssey','Plataforma',"10","ANY%","Media").
+juego('Super Mario Odyssey','Plataforma',"10","100%","Larga").
+juego('Ori and the Blind Forest','Plataforma',"10","ANY%","Corta").
+juego('Ori and the Blind Forest','Plataforma',"10","100%","Media").
+juego('What Remains of Edith Finch','Plataforma',"10","ANY%","Media").
+juego('What Remains of Edith Finch','Plataforma',"10","100%","Media").
+juego('Kirby: Star Allies','Plataforma',"10","ANY%","Media").
+juego('Kirby: Star Allies','Plataforma',"10","100%","Larga").
+juego('Super Mario 3D World','Plataforma',"10","ANY%","Larga").
+juego('Super Mario 3D World','Plataforma',"10","100%","Larga").
+juego('Geometry Dash','Plataforma',"10","ANY%","Media").
+juego('Geometry Dash','Plataforma',"10","100%","Media").
+juego('Cuphead','Plataforma',"10","ANY%","Corta").
+juego('Cuphead','Plataforma',"10","100%","Media").
+juego('Celeste','Plataforma',"10","ANY%","Corta").
+juego('Celeste','Plataforma',"10","100%","Larga").
+juego('Jump King','Plataforma',"10","ANY%","Corta").
+juego('Jump King','Plataforma',"10","100%","Media").
+
 juego('Elden Ring','Accion',"10","ANY%","Corta").
 juego('Elden Ring','Accion',"10","100%","Larga").
-juego('Assassins Creed Odyssey','Aventura',"10","ANY%","Larga").
-juego('Assassins Creed Odyssey','Aventura',"10","100%","Larga").
-juego('NBA 2k21','Deporte',"10","ANY%","Corta").
-juego('NBA 2k21','Deporte',"10","100%","Media").
-juego('Forza Horizon 4','Coches',"10","ANY%","Corta").
-juego('Forza Horizon 4','Coches',"10","100%","Larga").
-juego('Dead Space','Terror',"10","ANY%","Media").
-juego('Dead Space','Terror',"10","100%","Larga").
 juego('Bloodborne','Accion',"10","ANY%","Corta").
 juego('Bloodborne','Accion',"10","100%","Media").
-juego('Rocket League','Deporte',"10","ANY%","Corta").
-juego('Rocket League','Deporte',"10","100%","Media").
-juego('Red Dead Redemption','Aventura',"10","ANY%","Larga").
-juego('Red Dead Redemption','Aventura',"10","100%","Larga").
-juego('Cuphead',"Plataforma","10","ANY%","Corta").
-juego('Cuphead',"Plataforma","10","100%","Media").
 juego('Metroid Dread','Accion',"10","ANY%","Media").
 juego('Metroid Dread','Accion',"10","100%","Larga").
 juego('Grand Theft Auto V','Accion',"10","ANY%%","Larga").
 juego('Grand Theft Auto V','Accion',"10","100%","Larga").
 juego('Dark Souls III','Accion',"10","ANY%","Media").
 juego('Dark Souls III','Accion',"10","100%","Media").
-juego('Outlast','Terror',"10","ANY%","Corta").
-juego('Outlast','Terror',"10","100%","Corta").
-juego('Resident Evil 3','Terror',"10","ANY%","Media").
-juego('Resident Evil 3','Terror',"10","100%","Media").
-juego('Five Nights at Freddys','Terror',"10","ANY%","Media").
-juego('Five Nights at Freddys','Terror',"10","100%","Media").
+juego('Hollow Knight','Accion',"10","ANY%","Media").
+juego('Hollow Knight','Accion',"10","100%","Media").
+juego('DOOM','Accion',"10","ANY%","Corta").
+juego('DOOM','Accion',"10","100%","Larga").
+
+juego('Assassins Creed Odyssey','Aventura',"10","ANY%","Larga").
+juego('Assassins Creed Odyssey','Aventura',"10","100%","Larga").
 juego('The Legend of Zelda: Breath of the Wild','Aventura',"10","ANY%","Corta").
 juego('The Legend of Zelda: Breath of the Wild','Aventura',"10","100%","Larga").
 juego('The Witcher 3: Wild Hunt','Aventura',"10","ANY%","Larga").
 juego('The Witcher 3: Wild Hunt','Aventura',"10","100%","Larga").
 juego('Journey to the Savage Planet','Aventura',"10","ANY%","Corta").
 juego('Journey to the Savage Planet','Aventura',"10","100%","Larga").
-juego('Geometry Dash',"Plataforma","10","ANY%","Media").
-juego('Geometry Dash',"Plataforma","10","100%","Media").
-juego('Kirby: Star Allies',"Plataforma","10","ANY%","Media").
-juego('Kirby: Star Allies',"Plataforma","10","100%","Larga").
-juego('Super Mario 3D World',"Plataforma","10","ANY%","Larga").
-juego('Super Mario 3D World',"Plataforma","10","100%","Larga").
+juego('Red Dead Redemption','Aventura',"10","ANY%","Larga").
+juego('Red Dead Redemption','Aventura',"10","100%","Larga").
+
+juego('Dead Space','Terror',"10","ANY%","Media").
+juego('Dead Space','Terror',"10","100%","Larga").
+juego('Outlast','Terror',"10","ANY%","Corta").
+juego('Outlast','Terror',"10","100%","Corta").
+juego('Resident Evil 3','Terror',"10","ANY%","Media").
+juego('Resident Evil 3','Terror',"10","100%","Media").
+juego('Five Nights at Freddys','Terror',"10","ANY%","Media").
+juego('Five Nights at Freddys','Terror',"10","100%","Media").
+
+juego('Forza Horizon 4','Coches',"10","ANY%","Corta").
+juego('Forza Horizon 4','Coches',"10","100%","Larga").
 juego('Need for Speed: Rivals','Coches',"10","ANY%","Media").
 juego('Need for Speed: Rivals','Coches',"10","100%","Larga").
 juego('Crash Team Racing Nitro Fueled','Coches',"10","ANY%","Media").
 juego('Crash Team Racing Nitro Fueled','Coches',"10","100%","Media").
+
 juego('Inazuma Eleven Strikers','Deporte',"10","ANY%","Media").
 juego('Inazuma Eleven Strikers','Deporte',"10","100%","Larga").
 juego('Mario Golf: Super Rush','Deporte',"10","ANY%","Larga").
 juego('Mario Golf: Super Rush','Deporte',"10","100%","Larga").
 juego('Mario Tennis Aces','Deporte',"10","ANY%","Media").
 juego('Mario Tennis Aces','Deporte',"10","100%","Media").
-juego('Hollow Knight','Accion',"10","ANY%","Media").
-juego('Hollow Knight','Accion',"10","100%","Media").
-juego('Ori and the Blind Forest',"Plataforma","10","ANY%","Corta").
-juego('Ori and the Blind Forest',"Plataforma","10","100%","Media").
-juego('What Remains of Edith Finch',"Plataforma","10","ANY%","Media").
-juego('What Remains of Edith Finch',"Plataforma","10","100%","Media").
-juego('DOOM','Accion',"10","ANY%","Corta").
-juego('DOOM','Accion',"10","100%","Larga").
+juego('NBA 2k21','Deporte',"10","ANY%","Corta").
+juego('NBA 2k21','Deporte',"10","100%","Media").
+juego('Rocket League','Deporte',"10","ANY%","Corta").
+juego('Rocket League','Deporte',"10","100%","Media").
