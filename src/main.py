739852e92b50303a8134862973abacefc82ff44f
ariel-madrid <ariel.argomedo@usach.cl>
2022-07-05 23:26:03 -0400

    Codigo terminado

--- src/main.py
+++ src/main.py
@@ -5,20 +5,31 @@ from tkinter import messagebox
 from pyswip import *
 from playsound import playsound
 import multiprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+import skfuzzy as fuzz
+from skfuzzy import control
+from random import randint 
+from random import seed 
+
+seed(1)
 
 prolog = Prolog()
 prolog.consult("base_conocimiento.pl")
 p = multiprocessing.Process(target=playsound, args=("mario.mp3",))
 
-def capturarInformacion(combo,combo2,radioValue,combo3):
+def capturarInformacion(combo,combo2,radioValue):
     juegos = []
     categorias = []
     speedrun = []
     preferenciaTiempo = combo.get()
-    preferenciaDecada = combo2.get()
+    preferenciaDecada = int(combo2.get())
     preferenciaNivelHabilidad = radioValue.get()
-    preferenciaCategoria = combo3.get()
-
+    #preferenciaCategoria = combo3.get()
+    
+    num_exp = 0
+    num_time = 0
+    num_dec = 0
     tmp = 0
 
     #Dependiendo de las entradas a traves de la interfaz, se realizan diferentes consultas a la base de conocimientos.
@@ -26,38 +37,57 @@ def capturarInformacion(combo,combo2,radioValue,combo3):
         tkinter.messagebox.showinfo(message="Debe seleccionar un nivel de habilidad", title="Alerta")
         tmp = 1
     if (preferenciaTiempo == "Menos de 30 minutos"):
-        preferenciaTiempo = "Corta"
+        preferenciaTiempo = "Corta" #Poner valor numerico
+        num_time = 14.5
+
     elif (preferenciaTiempo == "Entre 30 y 90 minutos"):
         preferenciaTiempo = "Media"
+        num_time = 40
+
     elif (preferenciaTiempo == "Más de 90 minutos"):
         preferenciaTiempo = "Larga"
+        num_time = 100
+
     else:
         preferenciaTiempo = "Seleccione"
+        num_time = 0
 
-    if (preferenciaDecada == "1990-1999"):
-        preferenciaDecada = "90"
-    elif (preferenciaDecada == "2000-2009"):
+    if (preferenciaDecada >= 1990 and preferenciaDecada < 2000):
+        num_dec = preferenciaDecada
+        preferenciaDecada = "90" #Poner valor numerico
+
+    elif (preferenciaDecada >= 2000 and preferenciaDecada < 2010):
+        num_dec = preferenciaDecada
         preferenciaDecada = "2000"
-    elif (preferenciaDecada == "2010-2019"):
+        
+    elif (preferenciaDecada >= 2010 and preferenciaDecada < 2020):
+        num_dec = preferenciaDecada
         preferenciaDecada = "10"
+        
     else:
         preferenciaDecada = "Seleccione"
+        num_dec = randint(1990,2019)
 
     if (preferenciaNivelHabilidad == "Novato"):
-        preferenciaNivelHabilidad = "ANY%"
+        preferenciaNivelHabilidad = "ANY%" #Poner valor numerico
+        num_exp = 2.5
+
     elif (preferenciaNivelHabilidad == "Experto"):
         preferenciaNivelHabilidad = "100%"
+        num_exp = 7.5
+
     else:
         preferenciaNivelHabilidad = ""
+        num_exp = 0
 
+    preferenciaCategoria = escogerCategoria(num_exp, num_time, num_dec)
+    
     if (preferenciaCategoria == "Accion"):
         preferenciaCategoria = "Accion"
     elif (preferenciaCategoria == "Aventura"):
         preferenciaCategoria = "Aventura"
     elif (preferenciaCategoria == "Plataforma"):
         preferenciaCategoria = "Plataforma"
-    elif (preferenciaCategoria == "Deporte"):
-        preferenciaCategoria = "Deporte"
     elif (preferenciaCategoria == "Coches"):
         preferenciaCategoria = "Coches"
     elif (preferenciaCategoria == "Terror"):
@@ -65,6 +95,8 @@ def capturarInformacion(combo,combo2,radioValue,combo3):
     else:
         preferenciaCategoria = "Seleccione"
 
+    
+    ''''''
     '''
     POSIBLES COMBINACIONES DE ENTRADAS PARA LAS CONSULTAS DE PROLOG
     juego(X,a,b,c,_) - 1
@@ -234,7 +266,10 @@ def ingresoSistema(inputNombre,ventana):
     decada.place(x=50, y=120)
 
     combo2 = ttk.Combobox()
-    combo2 = ttk.Combobox(state="readonly",values=["1990-1999","2000-2009","2010-2019","No es relevante"])
+    combo2 = ttk.Combobox(state="readonly",
+        values=["1990","1991","1992","1993","1994","1995","1996","1997","1998","1999",
+                "2000","2001","2002","2003","2004","2005","2006","2007","2008","2009",
+                "2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","No es relevante"])
     combo2.set("Seleccione")
     combo2.place(x=50, y=145)
 
@@ -251,15 +286,7 @@ def ingresoSistema(inputNombre,ventana):
     rdioNovato.place(x=50, y=210)
     rdioExperto.place(x=150, y=210)
 
-    categoria = tkinter.Label(selecciones,text="¿Es de su preferencia alguna categoria en especifico?",bg = "#88cffa")
-    categoria.place(x=50, y=240)
-
-    combo3 = ttk.Combobox()
-    combo3 = ttk.Combobox(state="readonly",values=["Accion","Aventura","Plataforma","Deporte","Coches","Terror","No es relevante"])
-    combo3.set("Seleccione")
-    combo3.place(x=50, y=265)
-
-    enviar = tkinter.Button(selecciones,text="Consultar",command=lambda: capturarInformacion(combo,combo2,radioValue,combo3))
+    enviar = tkinter.Button(selecciones,text="Consultar",command=lambda: capturarInformacion(combo,combo2,radioValue))
     enviar.place(x=510, y=340)
 
     apagar = tkinter.Button(selecciones, text="Apagar Música",command=lambda: stopMusic())
@@ -298,5 +325,79 @@ def root():
     #Se muestra la ventana
     ventana.mainloop()
 
+def escogerCategoria(exp_value, time_value, dec_value):
+    x_experiencia = np.arange(0,12,1)
+    x_tiempo = np.arange(0,120,1)
+    x_decada = np.arange(1990,2023,1)
+
+    x_categoria = np.arange(1,34,1)
+
+    experiencia = control.Antecedent(x_experiencia,"Experiencia")
+    tiempo = control.Antecedent(x_tiempo,"Tiempo")
+    decada = control.Antecedent(x_decada,"Decada")
+    categoria = control.Consequent(x_categoria,"Categoria",'centroid')
+
+    # Funciones de membresia
+    experiencia["Novato"] = fuzz.trimf(x_experiencia, [0, 0, 5])
+    experiencia["Experto"] = fuzz.trimf(x_experiencia, [5, 10, 10])
+
+    tiempo["Poco"] = fuzz.zmf(x_tiempo, 0, 29)
+    tiempo["Medio"] = fuzz.pimf(x_tiempo, 20, 35, 45, 60)
+    tiempo["Mucho"] = fuzz.smf(x_tiempo, 60, 120)
+
+    decada["Noventas"] = fuzz.trimf(x_decada, [1990,1995,2000])
+    decada["Dosmil"] = fuzz.trimf(x_decada, [2000,2005,2010])
+    decada["Dosmildiez"] = fuzz.trimf(x_decada, [2010,2015,2019])
+
+    categoria["Accion"] = fuzz.zmf(x_categoria, 0,7)
+    categoria["Plataforma"] = fuzz.pimf(x_categoria, 6,8,12,14)
+    categoria["Coches"] = fuzz.trimf(x_categoria, [14,18,21])
+    categoria["Aventura"] = fuzz.smf(x_categoria, 21,28)
+    categoria["Terror"] = fuzz.trimf(x_categoria, [29,33,33])
+
+    regla1 = control.Rule(tiempo["Poco"] & experiencia["Experto"] & decada["Noventas"], categoria["Terror"])
+    regla2 = control.Rule(tiempo["Mucho"] & experiencia["Novato"], categoria["Plataforma"])
+    regla3 = control.Rule(tiempo["Medio"] & experiencia["Experto"] & decada["Dosmildiez"], categoria["Coches"])
+    regla4 = control.Rule(tiempo["Mucho"] & experiencia["Novato"], categoria["Aventura"])
+    regla5 = control.Rule((tiempo["Poco"] | experiencia["Novato"]) & decada["Noventas"], categoria["Plataforma"])
+    regla6 = control.Rule(tiempo["Poco"] & experiencia["Experto"] & decada["Dosmil"], categoria["Plataforma"])
+    regla7 = control.Rule(tiempo["Mucho"] | experiencia["Experto"], categoria["Accion"])
+    regla8 = control.Rule(tiempo["Poco"] & experiencia["Novato"], categoria["Plataforma"])
+
+    categoriaCtrl = control.ControlSystem([regla1,regla2,regla3,regla4,regla5,regla6,regla7,regla8])
+    categoriaSim = control.ControlSystemSimulation(categoriaCtrl)
+
+    #Inputs
+    categoriaSim.input['Experiencia'] = exp_value
+    categoriaSim.input['Tiempo'] = time_value
+    categoriaSim.input['Decada'] = dec_value
+    categoriaSim.compute()
+    cat_value = categoriaSim.output['Categoria']
+
+    print(cat_value)
+
+    experiencia.view()
+    tiempo.view()
+    decada.view()
+    categoria.view()
+
+    categoria.view(sim=categoriaSim)
+
+    if(cat_value >= 0.0 and cat_value <= 6.5):
+        categoria = "Accion"
+    elif(cat_value > 6.5 and cat_value <= 14.0):
+        categoria = "Plataforma" 
+    elif(cat_value > 14.0 and cat_value <= 21.0):
+        categoria = "Coches"
+    elif(cat_value > 21.0 and cat_value <= 28.5):
+        categoria = "Aventura"
+    elif(cat_value > 28.5 and cat_value < 33):
+        categoria = "Terror"
+        
+    return categoria
+
 if __name__ == "__main__":
     root()
+
+
+
